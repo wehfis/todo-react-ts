e4d54faf106b2041d22407f332124c0ab3d2f0ec
wehfis <wehfis@gmail.com>
2024-04-02 19:25:00 +0300

    feat: add edit
    Implement edit operation.

--- src/Components/TaskCard/TaskCard.tsx
+++ src/Components/TaskCard/TaskCard.tsx
@@ -12,6 +12,8 @@ type TaskCardProps = {
 function TaskCard(props: TaskCardProps) {
     const { updateTask, deleteTask } = useTaskContext();
     const [cardHover, setCardHover] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedText, setEditedText] = useState(props.task.text);
 
     const toggleCompleted = useCallback(async () => {
         await updateTask(props.task.id, {
@@ -20,31 +22,62 @@ function TaskCard(props: TaskCardProps) {
         });
     }, [props]);
 
+
+    const handleUpdateTask = async () => {
+        if (isEditing){
+            if(editedText.trim()){
+                await updateTask(props.task.id, {
+                    ...props.task,
+                    text: editedText.trim(),
+                });
+                
+            } else{
+                await deleteTask(props.task.id);
+            }
+            setIsEditing(false);
+        }
+    };
+    
+    
     return (
         <>
-            <TaskCheckbox task={props.task} toggleCompleted={async () => await toggleCompleted()} />
+            <TaskCheckbox task={props.task} toggleCompleted={toggleCompleted} />
             <li
                 onMouseEnter={() => setCardHover(true)}
                 onMouseLeave={() => setCardHover(false)}
                 className={styles.card_item}
             >
-                <div className={styles.card_section}>
-                    <label
-                        className={`${styles.card_label} ${
-                            props.task.completed
-                                ? styles.card_item_completed
-                                : ''
-                        }`}
-                    >
-                        {props.task.text}
-                    </label>
+                <div className={styles.card_section} onDoubleClick={() => setIsEditing(true)}> 
+                    {isEditing ? (
+                        <input
+                            className={styles.edit_input}
+                            type="text"
+                            value={editedText}
+                            onChange={(e) => setEditedText(e.target.value)}
+                            onKeyDown={async (e) => {
+                                if (e.key === 'Enter') {
+                                    await handleUpdateTask();
+                                }
+                            }}
+                            onBlur={async () => await handleUpdateTask()}
+                            autoFocus
+                        />
+                    ) : (
+                        <label
+                            className={`${styles.card_label} ${
+                                props.task.completed ? styles.card_item_completed : ''
+                            }`}
+                        >
+                            {props.task.text}
+                        </label>
+                    )}
                 </div>
                     <span
                         onClick={async () => await deleteTask(props.task.id)}
                         className={styles.card_delete}
                 >
                     {cardHover &&
-                        '✖' }
+                        '✖️' }
                     </span>
             </li>
         </>
